54ca3996c093c726eeb5f800d93c70422ee8cebd
SalaamJ <leansalaam@gmail.com>
2020-02-26 19:27:17 -0500

    Oiue

---
+++ Restaurant/Restaurant/Restaurant.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant
+{
+    class Restaurant
+    {
+        public string Name;
+        public string Culture;
+        public string 
+    }
+}
