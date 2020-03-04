025693d565c11829e29bfe5fe4fc9240d929c435
SalaamJ <leansalaam@gmail.com>
2020-03-04 17:45:31 -0500

    Yeet

--- Week 3 Code Along/Week 3 Code Along/Dog.cs
+++ Week 3 Code Along/Week 3 Code Along/Dog.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Week_3_Code_Along
+namespace Week_3_code_along
 {
     class Dog
     {
@@ -20,7 +20,7 @@ namespace Week_3_Code_Along
 
         public void SayName()
         {
-            MessageBox.Show("My name is Sir " + Name + " Von Strauss.");
+            MessageBox.Show("My name is " + Name);
         }
     }
 }
