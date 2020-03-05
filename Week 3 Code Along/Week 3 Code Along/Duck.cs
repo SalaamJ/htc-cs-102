95098e9eed27539dc5afaab89ced2170f64ece7b
SalaamJ <leansalaam@gmail.com>
2020-03-04 19:16:59 -0500

    Honque and Movie

---
+++ Week 3 Code Along/Week 3 Code Along/Duck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Week_3_code_along
+{
+    class Goose
+    {
+        public string Name;
+        public int Age;
+        public int AttackDamage;
+
+        public void Honque()
+        {
+            MessageBox.Show("Honque Honque, my fluffy baguette. I am Monsieur " + Name + ". I am " + Age + " white flags old.");
+        }
+
+        public void Dueling()
+        {
+            MessageBox.Show("I jab at you with my sabre, dealing " + AttackDamage + " honques of damage.");
+        }
+    }
+}
