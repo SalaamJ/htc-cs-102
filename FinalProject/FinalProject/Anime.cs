e88ecececa7dfdd5d885181f75651e3ef54775d7
Salaam Jallad <leansalaam@gmail.com>
2020-04-22 19:07:12 -0400

    Anime List

---
+++ FinalProject/FinalProject/Anime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+
+namespace AnimeList
+{
+    public class Anime
+    {
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        public string Genre
+        {
+            get;
+            set;
+        }
+
+        public string Creator
+        {
+            get;
+            set;
+        }
+
+        public void ShowDetails()
+        {
+            string infoString = "Title: " + this.Title;
+            infoString = "Genre: " + this.Genre;
+            infoString = "Creator: " + this.Creator;
+            MessageBox.Show(infoString);
+        }
+
+        public Anime(string myTitle, string myGenre, string myCreator)
+        {
+            this.Title = myTitle;
+            this.Genre = myGenre;
+            this.Creator = myCreator;
+        }
+    }
+}
---
+++ FinalProject/FinalProject/MainWindow.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimeList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private ObservableCollection<Anime> AnimeList;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            AnimeList = new ObservableCollection<Anime>();
+            lvAnimes.ItemsSource = AnimeList;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            Anime animeToAdd = new Anime(titleInput.Text, genreInput.Text, creatorInput.Text);
+
+            AnimeList.Add(animeToAdd);
+
+            titleInput.Clear();
+            genreInput.Clear();
+            creatorInput.Clear();
+        }
+
+        private void ShowButton_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Anime m in AnimeList)
+            {
+                m.ShowDetails();
+            }
+        }
+
+        private void lvMovies_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Anime selectedAnime = lvAnimes.SelectedItem as Anime;
+            if (selectedAnime != null)
+            {
+                selectedAnime.ShowDetails();
+            }
+        }
+    }
+}
